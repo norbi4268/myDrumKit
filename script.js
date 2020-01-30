9b75b61cd13817f847386b6e89d6153e6b5b9858
norbi4268 <n.suder@icloud.com>
2020-01-30 22:23:31 +0100

    added keypush animation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   audio.currentTime = 0;
   audio.play();
   console.log(e);
+  key.classList.add('playing');
 });
